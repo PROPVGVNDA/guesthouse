ea5d90fcc50ee17d67a6e65ce9a0ce94b83f9a49
Mykyta Sherstnov <mykyta.sherstnov@gmail.com>
2024-04-23 20:42:22 +0300

    init

---
+++ src/test/java/com/university/guesthouse/GuesthouseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.university.guesthouse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuesthouseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
